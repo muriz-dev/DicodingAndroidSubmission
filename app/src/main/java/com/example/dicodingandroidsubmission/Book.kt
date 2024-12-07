1521de0bd033a044982cf941c53f46e6b18cddb9
Muhamad Rizki <mhmdrizki.dev@gmail.com>
2024-12-07 11:11:38 +0700

    Add star, rating, reviewer, and genre data to data class

--- app/src/main/java/com/example/dicodingandroidsubmission/Book.kt
+++ app/src/main/java/com/example/dicodingandroidsubmission/Book.kt
@@ -8,5 +8,9 @@ data class Book(
     val title: String,
     val author: String,
     val description: String,
-    val cover: String
+    val cover: String,
+    val star: String,
+    val rating: String,
+    val reviewer: String,
+    val genre: String
 ) : Parcelable
