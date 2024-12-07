76b6dd08e6c3bfe03f126443cd29e83257e9c66f
Muhamad Rizki <mhmdrizki.dev@gmail.com>
2024-12-07 12:01:00 +0700

    Recomend or share this book feature

--- app/src/main/java/com/example/dicodingandroidsubmission/BookDetail.kt
+++ app/src/main/java/com/example/dicodingandroidsubmission/BookDetail.kt
@@ -1,7 +1,10 @@
 package com.example.dicodingandroidsubmission
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -10,7 +13,12 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.bumptech.glide.Glide
 
-class BookDetail : AppCompatActivity() {
+class BookDetail : AppCompatActivity(), View.OnClickListener {
+    private lateinit var title: String
+    private lateinit var author: String
+    private lateinit var rating: String
+    private lateinit var genre: String
+
     companion object {
         const val EXTRA_BOOK = "extra_book"
     }
@@ -33,13 +41,21 @@ class BookDetail : AppCompatActivity() {
         val tvReviewer: TextView = findViewById(R.id.tv_reviewer)
         val tvDescription: TextView = findViewById(R.id.tv_description)
         val tvGenre: TextView = findViewById(R.id.tv_genre)
+        val btnShare: Button = findViewById(R.id.btn_share)
+
+        btnShare.setOnClickListener(this)
 
         val book = if (Build.VERSION.SDK_INT >= 33){
-            intent.getParcelableExtra<Book>(EXTRA_BOOK, Book::class.java)
+            intent.getParcelableExtra(EXTRA_BOOK, Book::class.java)
         } else {
             @Suppress("DEPRECATION")
-            intent.getParcelableExtra<Book>(EXTRA_BOOK)
+            intent.getParcelableExtra(EXTRA_BOOK)
         }
+
+        title = book?.title.toString()
+        author = book?.author.toString()
+        rating = "${book?.star} ${book?.rating}"
+        genre = book?.genre.toString()
 
         Glide.with(this).load(book?.cover).into(imgCover)
         tvTitle.text = book?.title.toString()
@@ -49,5 +65,24 @@ class BookDetail : AppCompatActivity() {
         tvReviewer.text = book?.reviewer.toString()
         tvDescription.text = book?.description.toString()
         tvGenre.text = book?.genre.toString()
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_share ->{
+                val opening = "Ada satu buku yang belakangan ini tidak bisa aku hentikan untuk tidak menceritakannya kepada setiap orang - dan kamu harus segera membacanya!\n\n"
+                val bookContent = "Title: ${title}\nAuthor: ${author}\nRating: ${rating}\nGenre: ${genre}\n\n"
+                val closing = "Meski tidak sempurna, buku ini menyimpan pesona tersendiri yang mungkin akan mengejutkanmu."
+
+                val content = opening + bookContent + closing
+                val shareBookIntent: Intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, content)
+                    type = "text/plain"
+                }
+                val shareIntent = Intent.createChooser(shareBookIntent, null)
+                startActivity(shareIntent)
+            }
+        }
     }
 }
